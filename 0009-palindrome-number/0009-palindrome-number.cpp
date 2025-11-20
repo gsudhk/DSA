69f752bf09ddfe4b1f22570466552b4939bab751
Gurusudhanva K <118593031+gsudhk@users.noreply.github.com>
2025-11-20 12:55:41 +0530

    Time: 3 ms (46.3%), Space: 8.5 MB (68.51%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int s = x;
+        long long y = 0;
+        while(x>0){
+            int rem = x%10;
+            x = x/10;
+            y = y * 10 + rem;
+        }
+        return (s == y);
+    }
+};
