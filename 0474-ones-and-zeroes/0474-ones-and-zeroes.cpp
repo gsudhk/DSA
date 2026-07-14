a1f938ee04e2dbf9c214d9c3a38e6170b1e6415e
Gurusudhanva K <118593031+gsudhk@users.noreply.github.com>
2026-07-14 20:56:39 +0530

    Time: 71 ms (69.02%), Space: 13.8 MB (76.81%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -9,21 +9,12 @@ public:
     }
     return count;
     }
-    int count0s(string s){
-        int count = 0;
-        for(int i =0;i<s.length();i++){
-            if(s[i] == '0'){
-                count++;
-            }
-    }
-    return count;
-    }
     int findMaxForm(vector<string>& strs, int m, int n) {
         int n1 = strs.size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         for(int i = 0;i<strs.size();i++){
             int ones = count1s(strs[i]);
-            int zero = count0s(strs[i]);
+            int zero = strs[i].length()-ones;
             for(int j = m;j>=zero;j--){
                 for(int k = n;k>=ones;k--){
                     int take = 1 + dp[j-zero][k-ones];
